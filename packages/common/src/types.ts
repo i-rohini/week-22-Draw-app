ad67f8844bc8783afefd08182b0a2da0525ab1cd
Rohini Garg <rohini2garg1@gmail.com>
2025-07-12 19:29:39 +0530

    init

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const CreateUserSchema = z.object({
   username: z.string().min(3).max(20),
-  passward: z.string(),
+  password: z.string(),
   name: z.string()
 })
 
